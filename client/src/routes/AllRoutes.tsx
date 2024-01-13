7c84abda03ed7c0350812ae1c5456f6a934f6d86
Karthik Alakunta <karthikalakunta21@gmail.com>
2024-01-13 19:16:55 +0530

    added review route

--- client/src/routes/AllRoutes.tsx
+++ client/src/routes/AllRoutes.tsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import Movies from "../components/Movies/Movies";
+import Reviews from "../components/Reviews/Reviews";
 const AllRoutes = () => {
   return (
     <Fragment>
       <Routes>
         <Route path="/" element={<Movies />} />
+        <Route path="/reviews/:id" element={<Reviews />} />
       </Routes>
     </Fragment>
   );
