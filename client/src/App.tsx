b0ed54b527d6496651e308160282f9f9eb26555e
Karthik Alakunta <karthikalakunta21@gmail.com>
2024-01-13 17:09:02 +0530

    removed unnecessary code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import { Fragment } from "react";
-import Navbar from "./components/Navbar";
 import AllRoutes from "./routes/AllRoutes";
 
 function App() {
   return (
     <Fragment>
-      <Navbar />
+      
       <AllRoutes />
     </Fragment>
   );
