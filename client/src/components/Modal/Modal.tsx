0f8f54224d0db4cc9cdcbe23f28efc9a8756d47a
Karthik Alakunta <karthikalakunta21@gmail.com>
2024-01-13 17:12:51 +0530

    modified styles

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -7,7 +7,7 @@ export default function Modal(props: ModalProps) {
   return (
     <Fragment>
       <div className="fixed left-0 top-0 z-20 flex h-screen w-full select-none items-center justify-center bg-slate-800 bg-opacity-50">
-        <div className="h-max  max-h-[calc(100%-50px)] w-full overflow-y-scroll whitespace-pre-wrap border-2 border-[#ccd7e0] bg-white p-4 md:mt-5 w-max">
+        <div className="h-max  max-h-[calc(100%-50px)] overflow-y-scroll whitespace-pre-wrap border-2 border-[#ccd7e0] bg-white p-4 md:mt-5 w-max">
           <div>
             <p
               className="ml-auto w-max cursor-pointer border"
